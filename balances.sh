63e31cd839632e6d1f36647b98994009fe458abc
Ilham Maulana <ilhamaulana24@gmail.com>
2019-12-05 19:29:39 +0700

    Create balances.sh

---
+++ balances.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+one_address=""
+while [$one_address == ""]
+do
+        read -p "Please provide your $ONE address:" one_address
+done
+
+one_directory=""
+while [$one_directory == ""]
+do
+        read -p "Please provide your $ONE ./hmy directory (default for root user is in root):" one_directory
+done
+
+echo -e "${GREEN} Please wait..."
+
+{
+        echo '#!/bin/bash'
+        echo ''
+		    echo 'echo Your wallet address is '$one_address''
+        echo 'cd '$one_directory''
+        echo './hmy --node="https://api.s0.p.hmny.io" balances '$one_address''
+} >balances.sh
+
+echo -e "${GREEN} Successed. Please run ./balances"
